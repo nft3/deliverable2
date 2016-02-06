4242c56f293cd529b59d631a09328c55ba7aa165
thetreuth <nft3@pitt.edu>
2016-02-06 13:25:38 -0500

    First commit

---
+++ src/deliverable/pkg2/Deliverable2.java
@@ -0,0 +1,16 @@
+package deliverable.pkg2;
+
+/**
+ *
+ * @author Nick Treu
+ */
+public class Deliverable2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
