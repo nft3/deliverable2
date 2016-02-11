f3c0b38dbc4059f00b5c66e5e844aece5cb4bc5a
thetreuth <nft3@pitt.edu>
2016-02-11 16:03:01 -0500

    Start of the Junit tests.

---
+++ test/deliverable/pkg2/Deliverable2Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deliverable.pkg2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nicholas Treu
+ */
+public class Deliverable2Test {
+    
+    public Deliverable2Test() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+}
